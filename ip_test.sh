9253db17249ec44f555c157539a014a27ab2ae1c
albertqu <albert_qu@berkeley.edu>
2018-06-12 09:01:12 -0700

    shell script

--- ip_test.sh
+++ ip_test.sh
@@ -1,9 +1,12 @@
-#!/bin/bash  
+#!/bin/bash
+touch ip_connected.txt  
 for ip in 192.168.0.{0..255}
   do   
     k=`ping -c 1 $ip | grep 'icmp'`
     if [ "$k" > 5 ]
-    then echo $ip
+    then 
+      echo $ip
+      echo $ip >> ip_connected.txt
     fi
   done
 
