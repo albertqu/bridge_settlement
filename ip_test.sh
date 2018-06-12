301f17c3caf5efcc8e80d6f3c95f4ff49c87443f
albertqu <albert_qu@berkeley.edu>
2018-06-12 09:19:58 -0700

    expedite the algorithm

--- ip_test.sh
+++ ip_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 touch ip_connected.txt  
-for ip in 192.168.0.{0..255}
+for ip in 192.168.0.{0..15}
   do   
-    k=`ping -c 1 $ip | grep 'icmp'`
-    if [ "$k" > 5 ]
+    s=`ping -c 1 $ip | grep 'icmp'`
+    check=`expr "$s" : '64 bytes'`
+    if [ "$check" -ge 5 ]
     then 
       echo $ip
       echo $ip >> ip_connected.txt
